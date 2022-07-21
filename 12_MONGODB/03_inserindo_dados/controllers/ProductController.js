8acdb0cd0cd08406d13ad53198511961326bb9fc
Derick Castro <derick1castro@gmail.com>
2022-07-21 15:00:33 -0300

    iniciando mongoDB

---
+++ 12_MONGODB/03_inserindo_dados/controllers/ProductController.js
@@ -0,0 +1,11 @@
+const Product = require('../models/Product')
+
+module.exports = class ProductController {
+    static showProducts(req, res) {
+        res.render('products/all')
+    }
+
+    static createProduct(req, res) {
+        res.render('products/create')
+    }
+}
